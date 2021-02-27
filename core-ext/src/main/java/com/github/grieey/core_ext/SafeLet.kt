bb45c617ba428171c9b4f6a279fbfb0f5f3f5d33
grieey <yangqing433520@163.com>
2021-02-27 16:34:16 +0800

    新增safelet相关的顶层方法

---
+++ core-ext/src/main/java/com/github/grieey/core_ext/SafeLet.kt
@@ -0,0 +1,103 @@
+package com.github.grieey.core_ext
+
+/**
+ * description: 空安全操作相关的操作符
+ * @date: 2019-12-03 18:15
+ * @author: Grieey
+ */
+
+fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
+    return if (p1 != null && p2 != null) block(p1, p2) else null
+}
+
+fun <T1 : Any, T2 : Any, T3 : Any, R : Any> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    block: (T1, T2, T3) -> R?
+): R? {
+    return if (p1 != null && p2 != null && p3 != null) block(p1, p2, p3) else null
+}
+
+fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, R : Any> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    p4: T4?,
+    block: (T1, T2, T3, T4) -> R?
+): R? {
+    return if (p1 != null && p2 != null && p3 != null && p4 != null) block(p1, p2, p3, p4) else null
+}
+
+fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, R : Any> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    p4: T4?,
+    p5: T5?,
+    block: (T1, T2, T3, T4, T5) -> R?
+): R? {
+    return if (p1 != null && p2 != null && p3 != null && p4 != null && p5 != null) block(
+        p1,
+        p2,
+        p3,
+        p4,
+        p5
+    ) else null
+}
+
+fun <T1 : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, R : Any> safeLet(
+    p1: T1?,
+    p2: T2?,
+    p3: T3?,
+    p4: T4?,
+    p5: T5?,
+    p6: T6?,
+    block: (T1, T2, T3, T4, T5, T6) -> R?
+): R? {
+    return if (p1 != null && p2 != null && p3 != null && p4 != null && p5 != null && p6 != null) block(
+        p1,
+        p2,
+        p3,
+        p4,
+        p5,
+        p6
+    ) else null
+}
+
+fun <T : Any, R : Any> Collection<T?>.whenAllNotNull(block: (List<T>) -> R) {
+    if (this.all { it != null }) {
+        block(this.filterNotNull())
+    }
+}
+
+fun <T : Any, R : Any> Collection<T?>.whenAnyNotNull(block: (List<T>) -> R) {
+    if (this.any { it != null }) {
+        block(this.filterNotNull())
+    }
+}
+
+/**
+ * 不为空的情况下增加判断
+ * @param T
+ * @param block
+ * @return
+ */
+inline fun <T> T?.notNullIf(block: (T) -> Boolean): Boolean {
+    return this?.let(block) ?: false
+}
+
+/**
+ * 枚举类使用valueOf如果找不到的话会抛出异常
+ * @param T
+ * @param type
+ * @param default
+ * @return
+ */
+inline fun <reified T : Enum<T>> valueOf(type: String, default: T): T {
+    return try {
+        java.lang.Enum.valueOf(T::class.java, type)
+    } catch (e: IllegalArgumentException) {
+        default
+    }
+}
